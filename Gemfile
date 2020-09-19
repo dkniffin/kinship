3feb90e659302610a13d0725de877921f0ffbe24
Derek Kniffin <derek@quimbee.com>
2020-09-18 23:14:10 -0400

    Add slim and configure webpack for CSS

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem "bootsnap", ">= 1.4.2", require: false
 gem "devise", "~> 4.6"
 gem "pg", ">= 0.18", "< 2.0"
 gem "puma", "~> 4.1"
+gem "slim-rails"
 gem "turbolinks", "~> 5"
 gem "webpacker", "~> 4.0"
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,32 @@
 const { environment } = require('@rails/webpacker')
 
+////////////////////////// sass/scss loader config //////////////////////////
+const sassLoader = environment.loaders.get('sass')
+const sassLoaderConfig = sassLoader.use.find(function (element) {
+  return element.loader === 'sass-loader'
+})
+
+const options = sassLoaderConfig.options
+options.implementation = require('sass') // Use Dart-implementation of Sass (default is node-sass)
+
+const globCssImporter = require('node-sass-glob-importer')
+options.sassOptions.importer = globCssImporter() // Add support for file globbing
+
+// Make everything in `abstract/*` available in all SCSS files
+sassLoader.use.push({
+  loader: 'sass-resources-loader',
+  options: {
+    resources: [
+      './app/javascript/styles/0_globals/*.scss'
+    ]
+  }
+})
+
+// Resolve url() statements to relative paths
+const cssLoaderIndex = sassLoader.use.findIndex((e) => e.loader === 'css-loader')
+sassLoader.use.splice(cssLoaderIndex + 1, 0, {
+  loader: "resolve-url-loader"
+})
+
+
 module.exports = environment
